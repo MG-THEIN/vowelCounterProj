9d3a8ec9b25b59348ddb07b9665b41d53248c41a
MG SOE THEIN <nnsoe4130code@gmail.com>
2022-04-09 09:10:46 -0400

    vowels counter project finished

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const textInput = document.querySelector('.textarea');
 const checkBtn = document.querySelector('.check-btn');
 
+checkBtn.addEventListener('click', countVowel);
+
+function countVowel(e){
+    e.preventDefault();
+
+    const splitString = textInput.value.toString().split("");
+    const vowels = [];
+    
+    splitString.forEach(e => {
+        if ((e == 'a')||(e == 'e')||(e == 'i')||(e == 'o')||(e == 'u')){
+            vowels.push(e);
+        }
+        else return;
+    });
+
+    alert(`There are ${vowels.length} vowels in this text.`);
+}
